623cad40059d9f8a1dd5f7e456e5a454d92824b1
Kens04 <kento090403@icloud.com>
2024-06-01 10:40:28 +0900

    Input入力コンポーネントを作成

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,4 @@
+import InputFiled from "@/app/features/auth/components/InputFiled";
 import React from "react";
 
 const Signup = () => {
@@ -5,9 +6,9 @@ const Signup = () => {
     <div className="mx-auto max-w-sm my-14">
       <h2 className="text-center font-medium text-2xl mb-4">新規登録</h2>
       <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="text" />
+        <InputFiled />
+        <InputFiled />
+        <InputFiled />
         <div className="mt-4">
           <button></button>
         </div>
---
+++ src/app/features/auth/components/InputFiled.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const InputFiled = () => {
+  return (
+    <div>
+      <label className="text-gray-700 text-sm font-bold mb-1 block mt-4">
+        ユーザー名
+      </label>
+      <input
+        type="text"
+        id="name"
+        placeholder="ユーザー名"
+        className="border rounded w-full shadow py-3 px-4 text-gray-700 leading-tight focus:outline-none"
+      />
+    </div>
+  );
+};
+
+export default InputFiled;
