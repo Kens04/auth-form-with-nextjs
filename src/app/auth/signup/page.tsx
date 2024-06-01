bcff1d5c84af75d7e09b0fe742e809cfb6b90e28
Kens04 <kento090403@icloud.com>
2024-06-01 10:47:20 +0900

    Propsを付与してコンポーネントを受け取る側で動的に変更

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -6,9 +6,24 @@ const Signup = () => {
     <div className="mx-auto max-w-sm my-14">
       <h2 className="text-center font-medium text-2xl mb-4">新規登録</h2>
       <form>
-        <InputFiled />
-        <InputFiled />
-        <InputFiled />
+        <InputFiled
+          label="ユーザー名"
+          name="username"
+          type="text"
+          placeholder="ユーザー名"
+        />
+        <InputFiled
+          label="メールアドレス"
+          name="email"
+          type="email"
+          placeholder="メールアドレス"
+        />
+        <InputFiled
+          label="パスワード"
+          name="password"
+          type="password"
+          placeholder="パスワード"
+        />
         <div className="mt-4">
           <button></button>
         </div>
--- src/app/features/auth/components/InputFiled.tsx
+++ src/app/features/auth/components/InputFiled.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-const InputFiled = () => {
+interface InputFiledProps {
+  name: string;
+  type: string;
+  label: string;
+  placeholder: string;
+}
+
+const InputFiled = ({name, label, type, placeholder}: InputFiledProps) => {
   return (
     <div>
-      <label className="text-gray-700 text-sm font-bold mb-1 block mt-4">
-        ユーザー名
+      <label htmlFor={name} className="text-gray-700 text-sm font-bold mb-1 block mt-4">
+        {label}
       </label>
       <input
-        type="text"
-        id="name"
-        placeholder="ユーザー名"
+        type={type}
+        id={name}
+        placeholder={placeholder}
         className="border rounded w-full shadow py-3 px-4 text-gray-700 leading-tight focus:outline-none"
       />
     </div>
