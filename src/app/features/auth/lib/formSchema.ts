40d657834bdf617c5ffa2b8a971914f40702489c
Kens04 <kento090403@icloud.com>
2024-06-01 11:38:50 +0900

    ログインページを作成

--- src/app/features/auth/lib/formSchema.ts
+++ src/app/features/auth/lib/formSchema.ts
@@ -10,3 +10,11 @@ export const signupFormSchema = z.object({
     .min(4, { message: "パスワードは4文字以上で入力してください。" })
     .max(10, { message: "パスワードは10文字以内で入力してください。" }),
 });
+
+export const loginFormSchema = z.object({
+  email: z.string().email({ message: "メールアドレスが正しくありません。" }),
+  password: z
+    .string()
+    .min(4, { message: "パスワードは4文字以上で入力してください。" })
+    .max(10, { message: "パスワードは10文字以内で入力してください。" }),
+});
