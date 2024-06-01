226710c659b6d0a74cf7b7db52ed132e88d9bc59
Kens04 <kento090403@icloud.com>
2024-06-01 11:06:02 +0900

    react-hook-formを導入してフォームの状態管理

---
+++ src/app/features/auth/hooks/useSignupForm.ts
@@ -0,0 +1,20 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export const useSignupForm = () => {
+  const form = useForm({
+    resolver: zodResolver(),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler = (data) => {
+    const { username, email, password } = data;
+    console.log(username, email, password);
+  };
+
+  return { form, onSubmit };
+};
